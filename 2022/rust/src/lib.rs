764a873b2523f50664076ed877482d770052337e
Andrew Morozko <16529201+Andrew-Morozko@users.noreply.github.com>
2022-12-02 05:17:11 +0400

    init

---
+++ 2022/rust/src/lib.rs
@@ -0,0 +1,32 @@
+use std::{
+    fs::File,
+    path::{Path, PathBuf},
+};
+
+use anyhow::{bail, Context, Result};
+
+pub fn data_dir() -> Result<PathBuf> {
+    let mut task_data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    if !task_data_dir.pop() {
+        bail!(
+            "Something wrong with build path {}",
+            task_data_dir.display()
+        );
+    }
+    task_data_dir.push("data");
+    let cur_exe = std::env::current_exe().context("Can't determine current executable")?;
+    let name = cur_exe
+        .file_name()
+        .context("Can't determine current executable's name")?;
+    task_data_dir.push(name);
+    if !task_data_dir.is_dir() {
+        bail!("Task data dir not found at {}", task_data_dir.display());
+    }
+    Ok(task_data_dir)
+}
+
+pub fn open(file_name: impl AsRef<Path>) -> Result<File> {
+    let mut path = data_dir()?;
+    path.push(file_name);
+    File::open(path).context("Can't open file")
+}
